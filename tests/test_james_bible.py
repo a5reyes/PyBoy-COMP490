d16b2f0c129ef1f44eba34e6453af3c768b2860e
a5reyes <A5REYES@student.bridgew.edu>
2026-04-08 22:42:33 -0400

    completed tests for james bible game
    Completed graphical and unit tests for james bible game

--- tests/test_james_bible.py
+++ tests/test_james_bible.py
@@ -1,36 +1,49 @@
 from pyboy import PyBoy
-import sys
-import trace
 
-# create a Trace object, telling it what to ignore, and whether to
-# do tracing or line-counting or both.
-tracer = trace.Trace(
-    ignoredirs=[sys.prefix, sys.exec_prefix],
-    trace=0,
-    count=1,
-    timing=True)
+def test_bible():
+    pyboy = PyBoy("./roms/bible.gb", window="SDL2")
+    try:
+        pyboy.button_press("START")
+        pyboy.tick(900, True) 
+        pyboy.button_press("START")
+        pyboy.tick(60, True)  
+        pyboy.button_press("START")
+        pyboy.tick(60, True) 
 
-def bible():
-    pyboy = PyBoy("./roms/bible.gb")
-    pyboy.button_press("start")
-    pyboy.tick(1, True)
-    pyboy.button_press("start")
-    pyboy.tick(1, True)
-    pyboy.button_press("right")
-    pyboy.tick(1, True)
-    pyboy.button_press("right")
-    pyboy.tick(1, True)
-    pyboy.button_press("right")
-    pyboy.tick(1, True)
-    pyboy.button_press("start")
-    pyboy.tick(1, True)
+        
+        assert pyboy.tilemap_background[7:17, 0] == [
+            328, 367, 364, 377, 288, 322, 361, 354,364, 357
+        ], "Expected 'Holy Bible' title screen"
+    finally:
+        pyboy.stop()
+
+def test_wisdom_tree():
+    pyboy = PyBoy("./roms/bible.gb", window="null")
+    cart = pyboy.mb.cartridge
+
+    expected_low = cart.rombanks[4, 0x0150]
+    expected_high = cart.rombanks[5, 0]
+
+    pyboy.mb.setitem(0x0002, 0x99)
+
+    assert cart.rombank_selected_low == 4
+    assert cart.rombank_selected == 5
+    assert pyboy.mb.getitem(0x0150) == expected_low
+    assert pyboy.mb.getitem(0x4000) == expected_high
+
+    print("PASS: bible.gb Wisdom Tree switching works")
     pyboy.stop()
 
+def test_multibank():
+    pyboy = PyBoy("./roms/bible.gb", window="null")
+    cart = pyboy.mb.cartridge
 
-# run the new command using the given tracer
-tracer.runfunc(bible)
+    expected_high = cart.rombanks[5, 0]
+    pyboy.mb.setitem(0x2000, 5)
 
+    assert cart.rombank_selected == 5
+    assert pyboy.mb.getitem(0x4000) == expected_high
 
-# make a report, placing output in the current directory
-r = tracer.results()
-r.write_results(show_missing=True, coverdir=".")
+    print("PASS: bible.gb ROMOnly multibank switching works")
+    pyboy.stop()
+
